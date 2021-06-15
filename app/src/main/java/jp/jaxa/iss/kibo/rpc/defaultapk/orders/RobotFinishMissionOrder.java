98444b74171a9f3f45795c29e617b360490fcb53
Remus Courtenay <remuscourtenay@gmail.com>
2021-06-15 20:59:00 +1200

    commented finish mission order

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/orders/RobotFinishMissionOrder.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/orders/RobotFinishMissionOrder.java
@@ -2,8 +2,22 @@ package jp.jaxa.iss.kibo.rpc.defaultapk.orders;
 
 import gov.nasa.arc.astrobee.Result;
 
+/**
+ * Implementation of RobotOrder concerning the order for the Kibo Robot to announce that it has
+ * completed it's mission by playing the audio clip 'astrobee.mp3' located in the res/raw/ folder
+ * As the API's reportMissionCompletion method does not return a Result object, this class will
+ * always return an empty RobotOrderResult.
+ *
+ * @author Remus Courtenay
+ * @version 0
+ * @since 1.8
+ */
 public class RobotFinishMissionOrder extends RobotOrder {
 
+    /**
+     * Implementation of the specific order. Attempts to run the API's reportMissionCompletion
+     * method and throws an exception if the order fails.
+     */
     @Override
     protected Result attemptOrderImplementation() {
         if (!api.reportMissionCompletion()) {

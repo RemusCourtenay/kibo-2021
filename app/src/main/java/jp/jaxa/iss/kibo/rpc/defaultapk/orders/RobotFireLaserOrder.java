7e0a5241cd45d68c128cf9149ce3c77d34bda5cd
osamakashif <osamakashif1@hotmail.com>
2021-06-20 16:51:32 +1200

    feat - implemented solvePnP
    Implemented solvePnP from calib3d

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/orders/RobotFireLaserOrder.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/orders/RobotFireLaserOrder.java
@@ -7,6 +7,11 @@ import jp.jaxa.iss.kibo.rpc.api.KiboRpcApi;
 import org.opencv.aruco.Aruco;
 import org.opencv.aruco.Board;
 import org.opencv.core.Mat;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
 import org.opencv.core.Size;
 
 import java.util.ArrayList;
@@ -35,13 +40,33 @@ class RobotFireLaserOrder extends RobotOrder {
         Mat ids = new Mat(4, 1, 0);
         Aruco.detectMarkers(image, Aruco.getPredefinedDictionary(markerDictionaryID), corners, ids);
         Board board = Board.create(corners, Aruco.getPredefinedDictionary(markerDictionaryID), ids);
+        /**
+         * Attempt with estimate pose board
+         */
         // Mat counter = new Mat(4, 1, 0);
         // Size imageSize = new Size();
-        Mat distCoeffs = new Mat(4, 1, 0);
-        Mat rvecs = new Mat(4, 1, 0);
-        Mat tvecs = new Mat(4, 1, 0);
+        Mat distCoeffs = new Mat();//4, 1, 0);
+        Mat rvecs = new Mat();
+        Mat tvecs = new Mat();
         // double vecs = Aruco.calibrateCameraAruco(corners, ids, counter, board, imageSize, image, distCoeffs);
         int poses = Aruco.estimatePoseBoard(corners, ids, board, image, distCoeffs, rvecs, tvecs);
+        //Mat camMat = cameraMatrix(fc, new Size(frameSize.width / 2, frameSize.height / 2));
+        /**
+         * Attempt with calib3d solvePnP
+         */
+        Mat camMat = image;
+        MatOfDouble coeff = new MatOfDouble(); // dummy
+
+        MatOfPoint2f centers = new MatOfPoint2f();
+        //Size patternSize = new Size(4, 11);
+        //MatOfPoint3f grid = asymmetricalCircleGrid(patternSize);
+        MatOfPoint3f grid = new MatOfPoint3f(image);
+        Mat rvec = new MatOfFloat();
+        Mat tvec = new MatOfFloat();
+
+        MatOfPoint2f reprojCenters = new MatOfPoint2f();
+        Calib3d.solvePnP(grid, centers, camMat, coeff, rvec, tvec, false);
+        //Calib3d.solvePnP();
         return null; // TODO...
     }
 
